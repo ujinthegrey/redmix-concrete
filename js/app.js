2dc37094d7cf91d7f9a0c78e77cdfae2446c532a
ujinthegrey <v4deus@gmail.com>
2022-08-10 13:54:32 +0400

    add bg to 3rd section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// ---DHEADER  ROPDOWN -------------
 const menuIcon = document.querySelector('.menu__icon')
 const dropdown = document.querySelector('.dropdown')
 
@@ -5,35 +6,27 @@ if (menuIcon) {
     menuIcon.addEventListener('click', (e) => {
         e.preventDefault()
         menuIcon.classList.toggle('_active')
-        dropdown.classList.toggle('_active')
-        
+        dropdown.classList.toggle('_active')        
     })
 }
 
-
-/* --SMOOTH SCROOL --------------- */
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
-if (menuLinks) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener('click', onMenuLinkClick)
-    })
-
-    function onMenuLinkClick(e) {
-        const menuLink = e.target
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto)
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight
-
-            if (menuIcon.classList.contains('_active')) {
+// ---SMOOTH ANCHOR SCROOL-------------
+const anchors = document.querySelectorAll('a[href*="#"]')
+if (anchors.length) {
+    for (let anchor of anchors) {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault()
                 menuIcon.classList.remove('_active')
-                dropdown.classList.remove('_active')
-            }
-
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: 'smooth'
+                dropdown.classList.remove('_active')  
+                
+                const blockToScroll = document.getElementById(anchor.getAttribute('href').substr(1))
+                
+                if (blockToScroll) {
+                    blockToScroll.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    })
+                }
             })
-            e.preventDefault()
         }
     }
-}
